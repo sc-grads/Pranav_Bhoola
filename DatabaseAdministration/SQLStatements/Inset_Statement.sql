b1f4341e2c5b7eb345c271c9ccce96cc7814857b
Pranav Bhoola <pranav.bhoola@sambeconsulting.com>
2024-02-29 16:10:57 +0200

    Added Insert Statement

---
+++ DatabaseAdministration/SQLStatements/Inset_Statement.sql
@@ -0,0 +1,6 @@
+INSERT INTO tableName (column1,column2,column3....column) VALUES (value1,value2,value3.....valueN)
+INSERT INTO StaffSale (staddId,fName,lName) VALUES (100,'Kohn','King')
+INSERT INTO SalesStaff1 VALUES (200,'Michael','Blythe'),
+                                (300,'Linda','Mitchell'),
+                                (400,'Jillian','Carson'),
+                                (500,'Garrett','Vargas');
